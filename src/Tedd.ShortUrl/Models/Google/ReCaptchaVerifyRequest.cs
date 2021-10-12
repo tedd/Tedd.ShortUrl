65299d9cd702fe6d5bc0eb9826e8a48e2b0728f3
Tedd Hansen <tedd@konge.net>
2021-10-12 20:11:48 +0200

    Delete file fixes #8

--- src/Tedd.ShortUrl/Models/Google/ReCaptchaVerifyRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Tedd.ShortUrl.Models.Google
-{
-    public class ReCaptchaVerifyRequest
-    {
-        public string secret { get; set; }
-        public string response { get; set; }
-    }
-}
