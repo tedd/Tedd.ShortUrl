a48aca569a2be1e2eca1560eeb9b41bfc6229fb2
Tedd Hansen <tedd@konge.net>
2021-10-13 09:25:35 +0200

    Docker support fixes #13

--- src/Tedd.ShortUrl/Program.cs
+++ src/Tedd.ShortUrl/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -18,6 +19,11 @@ namespace Tedd.ShortUrl
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddCommandLine(args);
+                    config.AddEnvironmentVariables();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
